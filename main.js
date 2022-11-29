d70027f3720f65e5e590cadaa15a3cb40c3a29a9
Bayu Candra <110009488+Bayu-x3@users.noreply.github.com>
2022-11-29 13:34:35 +0700

    Update main.js
    menambahkan pilihan

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-test
+const input = require("readline-sync");
+console.log("-----------------------------");
+console.log(       "1. Penjumalahan       ");
+console.log(       "2. Pengurangan        ");
+console.log(       "3. Perkalian          ");
+console.log(       "4. Pembagian          ");
+console.log("-----------------------------");
+console.log(" ")
+
+const pil = input.question("Silahkan pilih nomor: ");
